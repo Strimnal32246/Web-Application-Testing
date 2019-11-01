af298b55976f557c513d8baac1aeae4aedbe3ce6
strimnal32246 <susan.trimnal@icdccollege.edu>
2019-11-01 11:23:08 -0400

    addded display, dashboard, and app

---
+++ baseball/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+import Display from "./Components/Display.js";
+import Dashboard from "./Components/Dashboard.js";
+
+function App(props) {
+  const clearCount = { strikes: 0, balls: 0 };
+  const [count, setCount] = useState({ ...clearCount, ...props.count });
+  const resetCount = () => setCount(clearCount);
+  const strike = () =>
+    count.strikes >= 2
+      ? resetCount()
+      : setCount({ ...count, strikes: count.strikes + 1 });
+  const ball = () =>
+    count.balls >= 3
+      ? resetCount()
+      : setCount({ ...count, balls: count.balls + 1 });
+  const foul = () =>
+    setCount({ ...count, strikes: Math.min(count.strikes + 1, 2) });
+  const hit = () => resetCount();
+  return (
+    <div className="App">
+      <h2>I Dont watch BaseBall!</h2>
+      <Display strikes={count.strikes} balls={count.balls} />
+      <Dashboard strike={strike} ball={ball} foul={foul} hit={hit} />
+    </div>
+  );
+}
+
+export default App;
